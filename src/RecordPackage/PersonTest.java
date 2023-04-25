a7e2f43b19d51bc9453a711ef56422c82fd7b4b3
kenta-saito <k.saito@tc-tech.co.jp>
2023-04-25 16:02:37 +0900

    Personレコードの拡張-並び替え（終了）

--- src/RecordPackage/PersonTest.java
+++ src/RecordPackage/PersonTest.java
@@ -1,14 +1,21 @@
 package RecordPackage;
 
+import java.util.Arrays;
+
 //レコードクラス-1-2
 public class PersonTest {
-    public static void main(String[] args){
-        record person(String name, String furigana, int age){}
+    public static void main(String[] args) {
+        record person(String name, String furigana, int age) implements Comparable {
+            @Override
+            public int compareTo(Object o) {
+                return this.age - ((person) o).age;
+            }
+        }
 
         /**
          * ５人配列で生成
          */
-        person[] persons ={
+        person[] persons = {
                 new person("斎藤", "サイトウ", 28),
                 new person("後藤", "ゴトウ", 30),
                 new person("佐藤", "サトウ", 14),
@@ -21,42 +28,43 @@ public class PersonTest {
          */
         int ageMax = 0;
         int ageMaxAns = 0;
-        for(int i = 0; i < persons.length; i++){
-            if(ageMax < persons[i].age){
+        for (int i = 0; i < persons.length; i++) {
+            if (ageMax < persons[i].age) {
                 ageMax = persons[i].age;
                 ageMaxAns = i;
             }
         }
-        System.out.println("一番年齢の高い人は"+persons[ageMaxAns].name+
-                "さんの"+persons[ageMaxAns].age+"歳");
+        System.out.println("一番年齢の高い人は" + persons[ageMaxAns].name +
+                "さんの" + persons[ageMaxAns].age + "歳");
 
         /**
          * 一番年齢の低い人を調べる
          */
         int ageMin = ageMax;
         int ageMinAns = 0;
-        for(int i = 0; i < persons.length; i++){
-            if(ageMin > persons[i].age){
+        for (int i = 0; i < persons.length; i++) {
+            if (ageMin > persons[i].age) {
                 ageMin = persons[i].age;
                 ageMinAns = i;
             }
         }
-        System.out.println("一番年齢の低い人は"+persons[ageMinAns].name+
-                "さんの"+persons[ageMinAns].age+"歳");
+        System.out.println("一番年齢の低い人は" + persons[ageMinAns].name +
+                "さんの" + persons[ageMinAns].age + "歳");
 
         /**
          * 年齢順に並べ替え
          */
-        for(int i = 0; i < persons.length-1; i++){
-            for(int j = i+1; j < persons.length; j++){
-                if(persons[j].age < persons[i].age){
-                    var num = persons[i];
-                    persons[i] = persons[j];
-                    persons[j] = num;
-                }
-            }
-        }
-        for (var value : persons){
+//        for (int i = 0; i < persons.length - 1; i++) {
+//            for (int j = i + 1; j < persons.length; j++) {
+//                if (persons[j].age < persons[i].age) {
+//                    var num = persons[i];
+//                    persons[i] = persons[j];
+//                    persons[j] = num;
+//                }
+//            }
+//        }
+        Arrays.sort(persons);
+        for (var value : persons) {
             System.out.println(value);
         }
     }
